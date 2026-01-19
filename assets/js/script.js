9cb6342a4498c3fc0071c79e536a69646f03273b
ThiagoAmaralDev <thiagoamaraldev@gmail.com>
2026-01-19 17:30:21 -0300

    Adicionando funcionalidades básicas em javascript (usando DOM, lógica de programação e eventos)

---
+++ assets/js/script.js
@@ -0,0 +1,54 @@
+
+
+/* HERO */
+
+/* SELETOR DE CORES */
+
+const imagemVisualizacao = document.getElementById('imagem-fone');
+const tituloVisualizacao = document.getElementById('titulo-produto');
+
+const botaoAzul = document.getElementById('btn-azul');
+const botaoBranco = document.getElementById('btn-branco');
+const botaoPreto = document.getElementById('btn-preto');
+
+function trocarImagem(caminho, nomeDaCor) {
+    imagemVisualizacao.src = caminho;
+    tituloVisualizacao.innerHTML = "Mergulhe no Silêncio - " + nomeDaCor;;
+}
+
+botaoAzul.addEventListener('click', function() {
+
+    trocarImagem('assets/img/fone/sonicone-azul.jpg', 'SonicOne Azul')
+});
+
+botaoBranco.addEventListener('click', function() {
+
+    trocarImagem('assets/img/fone/sonicone-branco.jpg', 'SonicOne Branco')
+});
+
+botaoPreto.addEventListener('click', function() {
+
+    trocarImagem('assets/img/fone/sonicone-preto.jpg', 'SonicOne Preto')
+});
+
+/* FOOTER */
+
+// MUDANÇA AUTOMÁTICA DO ANO 
+
+const elementoCopyright = document.getElementById("texto-copyright");
+
+const dataAtual = new Date();
+const anoLancamento = 2026;
+const anoAtual = dataAtual.getFullYear();
+
+
+if(anoAtual > anoLancamento) {
+   elementoCopyright.innerHTML = `&copy ${anoLancamento} - ${anoAtual} Sonic Audio. Feito por Thiago Amaral.`;
+} else {
+    elementoCopyright.innerHTML = `&copy ${anoAtual} Sonic Audio. Feito por Thiago Amaral.`;
+}
+
+
+
+
+
